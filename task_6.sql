b26a30314741d92133e81360a5bb6fafd4f26972
Aleksey <sport104@yandex.ru>
2026-06-26 04:54:21 +0300

    Update task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -1,1 +1,20 @@
-/*добавьте сюда запросы для решения задания 6*/
+-- task_6
+
+/* Используем блокировку FOR UPDATE
+ потому что FOR NO KEY UPDATE совместима с FOR KEY SHARE
+ и блокировка с режимом FOR KEY SHARE сможет читать данные строк,
+ на которые наложена FOR NO KEY UPDATE*/
+
+BEGIN;
+
+SELECT *
+FROM cafe.restaurants
+WHERE menu -> 'Кофе' -> 'Капучино' IS NOT NULL
+FOR UPDATE
+NOWAIT;
+
+UPDATE cafe.restaurants
+SET menu = jsonb_set(menu, '{Кофе, Капучино}', (((menu -> 'Кофе' -> 'Капучино')::numeric * 1.2)::text)::jsonb)
+WHERE menu -> 'Кофе' -> 'Капучино' IS NOT NULL;
+
+COMMIT;
