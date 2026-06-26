2f120de3bf4e992e666c6647beb34affdda457d2
Aleksey <sport104@yandex.ru>
2026-06-26 04:53:35 +0300

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,1 +1,21 @@
-/*добавьте сюда запрос для решения задания 4*/
+-- task_4
+
+WITH pizzas_set AS (
+	SELECT
+		cafe_name,
+		jsonb_object_keys(menu -> 'Пицца') AS pizzas
+	FROM cafe.restaurants
+	WHERE cafe_type = 'pizzeria'
+)
+SELECT
+	cafe_name,
+	pizzas_cnt
+FROM (
+	SELECT
+		cafe_name,
+		count(pizzas) AS pizzas_cnt,
+		RANK () OVER (ORDER BY count(pizzas) DESC) AS rating
+	FROM pizzas_set
+	GROUP BY cafe_name
+) AS pizzas_scores
+WHERE rating = 1;
